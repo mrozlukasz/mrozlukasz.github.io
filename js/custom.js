a87e8af806e759742f5511aaf887c4f1dfac92a4
Łukasz Mróz <l.mroz88@gmail.com>
2019-05-20 16:27:59 +0200

    Added Cookie Consent

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#444"
+    },
+    "button": {
+      "background": "#87D37C"
+    }
+  }
+})});
